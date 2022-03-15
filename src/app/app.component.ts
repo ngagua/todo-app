bb4207fe84a904884d7c270e3168a202928ab9d9
Nikusha <ngagua@gmail.com>
2022-03-15 13:07:00 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+export interface Todo {
+  content: string,
+  completed: boolean
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todos: Todo[] = []
+  inProgress: Todo[] = []
+  done: Todo[] = []
+  inputTodo: string = "";
+  selectedValue = null;
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+    this.inputTodo = "";
+    
+  }
+removeTask(id:number) {
+  this.todos = this.todos.filter((task, index) => index !==id);
+  
+}
+
+  moveToProgress (id:number) {
+    let inprog = this.todos.filter((task, index) => index ==id);
+    this.inProgress.push(...inprog);
+    this.todos = this.todos.filter((task, index) => index !==id);
+  }
+  ProgressTotodo (id:number) {
+    let inprog = this.inProgress.filter((task, index) => index ==id);
+    this.todos.push(...inprog);
+    this.inProgress = this.inProgress.filter((task, index) => index !==id);
+  }
+  moveToDone (id:number) {
+    let don = this.inProgress.filter((task, index) => index ==id);
+    this.done.push(...don);
+    this.inProgress = this.inProgress.filter((task, index) => index !==id);
+  }
+  doneToProgress (id:number) {
+    let don = this.done.filter((task, index) => index ==id);
+    this.inProgress.push(...don);
+    this.done = this.done.filter((task, index) => index !==id);
+  }
 }
