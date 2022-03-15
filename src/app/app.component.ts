e6a4994182cb8324925859c322a9c4273184dba5
Nikusha <ngagua@gmail.com>
2022-03-15 13:14:13 -0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   inProgress: Todo[] = []
   done: Todo[] = []
   inputTodo: string = "";
-  selectedValue = null;
+  selectedValue:string = "";
 
   addTodo() {
     this.todos.push({
